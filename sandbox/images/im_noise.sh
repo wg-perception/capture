c92c60ea3dafb7d3e30cdb900e8e04c8a2ef8ae5
Ethan Rublee <erublee@lou.willowgarage.com>
2011-12-07 13:50:43 -0800

    Working on a bit of sample.

--- sandbox/images/im_noise.sh
+++ sandbox/images/im_noise.sh
@@ -1,3 +1,4 @@
-convert -size 500x500  plasma:fractal fractal.png
+convert -size 850x1100 -density 100x100 \
+  plasma:fractal fractal.png
 convert fractal.png  -charcoal 10 -paint 10 -sigmoidal-contrast 15x50% -negate \
            result.png
