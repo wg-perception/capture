1cc48bd4ab825602ca7f65e57ffdda33ff874523
Vincent Rabaud <vrabaud@willowgarage.com>
2012-03-26 23:34:29 -0700

    let cakin handle the Python and make sure the tests pass

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='Capture',
+      version='1.0.0',
+      description='A simple set of scripts to capture a 3d model of an object',
+      packages=['capture'],
+      package_dir={'':'python'}
+)
