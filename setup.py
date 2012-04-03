442bb2636042c50c01b0bd628a1cdd91510c74fa
Vincent Rabaud <vrabaud@willowgarage.com>
2012-04-03 06:29:59 -0700

    add one more Python folder

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from distutils.core import setup
 setup(name='Capture',
       version='1.0.0',
       description='A simple set of scripts to capture a 3d model of an object',
-      packages=['capture'],
+      packages=['capture', 'cpture.arbotix'],
       package_dir={'':'python'}
 )
