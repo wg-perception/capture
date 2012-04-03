1783d5f324179e400bfc72071d88ca501d6f3eac
Vincent Rabaud <vrabaud@willowgarage.com>
2012-04-03 08:40:17 -0700

    fix typo

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from distutils.core import setup
 setup(name='Capture',
       version='1.0.0',
       description='A simple set of scripts to capture a 3d model of an object',
-      packages=['capture', 'cpture.arbotix'],
+      packages=['capture', 'capture.arbotix'],
       package_dir={'':'python'}
 )
