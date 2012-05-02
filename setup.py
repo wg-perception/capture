dd54d159dda2e655165a8687d7d9ac0bc481460d
Vincent Rabaud <vrabaud@willowgarage.com>
2012-05-02 05:48:54 -0700

    better name for egg

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-setup(name='Capture',
+setup(name='object_recognition_capture',
       version='1.0.0',
       description='A simple set of scripts to capture a 3d model of an object',
       packages=['object_recognition_capture', 'object_recognition_capture.arbotix'],
