4a856258f6116407513468f6caacc062f781ba31
Vincent Rabaud <vrabaud@willowgarage.com>
2012-10-22 11:22:31 -0700

    comply to the new API

--- apps/orb_template.py
+++ apps/orb_template.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 from object_recognition_capture.orb_capture import *
 from ecto.opts import scheduler_options, run_plasm
-from ecto_opencv.calib import PoseDrawer, DepthValidDraw
+from ecto_opencv.calib import PoseDrawer, DepthValidDraw, DepthTo3d
 from ecto_opencv.features2d import DrawKeypoints
 from ecto_opencv.highgui import imshow, FPSDrawer, MatWriter, ImageSaver
 from ecto_opencv.imgproc import cvtColor, Conversion
-from ecto_openni import SXGA_RES, FPS_15
+from ecto_openni import VGA_RES, SXGA_RES, FPS_15, FPS_30
 from ecto_image_pipeline.io.source import create_source
 import argparse
 import ecto
@@ -31,7 +31,7 @@ plasm = ecto.Plasm()
 #setup the input source, grayscale conversion
 source = create_source('image_pipeline','OpenNISource',image_mode=SXGA_RES,image_fps=FPS_15)
 rgb2gray = cvtColor (flag=Conversion.RGB2GRAY)
-
+depth_to_3d = DepthTo3d()
 plasm.connect(source['image'] >> rgb2gray ['image'])
 
 #convenience variable for the grayscale
@@ -44,8 +44,10 @@ plasm.connect(source['depth'] >> imshow(name='depth')[:],
 #connect up the test ORB
 orb = FeatureFinder('ORB test', n_features=options.n_features, n_levels=3, scale_factor=1.2)
 plasm.connect(img_src >> orb['image'],
-              source['points3d'] >> orb['points3d'],
-              source['mask'] >> orb['mask']
+              source['depth_raw'] >> depth_to_3d['depth'],
+              source['K'] >> depth_to_3d['K'],
+              depth_to_3d['points3d'] >> orb['points3d'],
+              source['mask_depth'] >> orb['mask']
               )
 
 
@@ -56,14 +58,15 @@ orb_display = imshow('orb display', name='ORB', triggers=dict(save=ord('s')))
 depth_valid_draw = DepthValidDraw()
 plasm.connect(orb['keypoints'] >> draw_kpts['keypoints'],
               source['image'] >> depth_valid_draw['image'],
-              source['mask'] >> depth_valid_draw['mask'],
+              source['mask_depth'] >> depth_valid_draw['mask'],
               depth_valid_draw['image'] >> draw_kpts['image'],
               draw_kpts['image'] >> fps[:],
              )
 
 plane_est = PlaneEstimator(radius=50)
 pose_draw = PoseDrawer()
-plasm.connect(source['image', 'points3d'] >> plane_est['image', 'points3d'],
+plasm.connect(source['image'] >> plane_est['image'],
+              depth_to_3d['points3d'] >> plane_est['points3d'],
               plane_est['R', 'T'] >> pose_draw['R', 'T'],
               source['K'] >> pose_draw['K'],
               fps[:] >> pose_draw['image'],
